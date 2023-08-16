fb49de8c981e5c8407cbdd2b45850da0b618b118
DanyloBag <bagryancevdanil@gmail.com>
2023-08-17 01:04:12 +0200

    commit 1

--- src/ex1/NameFormatter.java
+++ src/ex1/NameFormatter.java
@@ -3,20 +3,14 @@ package ex1;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class NameFormatter {
 
     public  String formatNames(List<String> names) {
-        HashMap<Integer, String> namesMap = new HashMap<>();
-
-        for (int i = 0; i < names.size(); i++) {
-            if (i % 2 != 0) {
-                namesMap.put(i,names.get(i));
-            }
-        }
-
-        return namesMap.entrySet().stream()
-                .map(entry -> entry.getKey() + ". " + entry.getValue())
+        return IntStream.range(0,names.size())
+                .filter(index -> index %2 ==0)
+                .mapToObj(index -> (index + 1) + ". " + names.get(index))
                 .collect(Collectors.joining(", "));
     }
 
