38d772ae5c0f5797ec2bb99ad03e3a0b9a01fd44
DanyloBag <132061463+DanyloBag@users.noreply.github.com>
2023-08-18 11:50:43 +0200

    Update NameFormatter.java

--- src/ex1/NameFormatter.java
+++ src/ex1/NameFormatter.java
@@ -7,8 +7,8 @@ import java.util.stream.IntStream;
 public class NameFormatter {
 
     public  String formatNames(List<String> names) {
-        return IntStream.range(0,names.size())
-                .filter(index -> index %2 ==0)
+        return IntStream.range(1,names.size())
+                .filter(index -> index %2 !=0)
                 .mapToObj(index -> (index + 1) + ". " + names.get(index))
                 .collect(Collectors.joining(", "));
     }
